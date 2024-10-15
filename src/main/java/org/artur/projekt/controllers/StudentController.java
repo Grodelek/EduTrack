109c0dda7b7e92e7975b3ac9d108013feae488bd
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-10-15 21:39:52 +0200

    Add files via upload

--- src/main/java/org/artur/projekt/controllers/StudentController.java
+++ src/main/java/org/artur/projekt/controllers/StudentController.java
@@ -1,4 +1,5 @@
 package org.artur.projekt.controllers;
+
 import jakarta.validation.Valid;
 import org.artur.projekt.models.Student;
 import org.artur.projekt.service.StudentService;
@@ -9,12 +10,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/students")
 public class StudentController {
 
-    private StudentService studentService;
+    private final StudentService studentService;
 
     @Autowired
     public StudentController(StudentService studentService) {
@@ -46,6 +48,25 @@ public class StudentController {
     @GetMapping("/delete")
     public String delete(@RequestParam("studentsId") long id){
         studentService.removeStudentById(id);
+        return "redirect:/students/result";
+    }
+
+    @GetMapping("/update")
+    public String getUpdate(@RequestParam("studentsId") long id, Model model){
+        Optional<Student> foundStudent = studentService.findStudentById(id);
+        if(foundStudent.isPresent()) {
+            model.addAttribute("student", foundStudent.get());
+            return "updateStudent";
+        }
+        return "redirect:/students/result";
+    }
+
+    @PostMapping("/update")
+    public String update(@Valid @ModelAttribute ("student") Student student, BindingResult result){
+        if(result.hasErrors()){
+            return "updateStudent";
+        }
+        studentService.saveStudent(student);
         return "redirect:/students/result";
     }
 }
--- src/main/java/org/artur/projekt/service/StudentService.java
+++ src/main/java/org/artur/projekt/service/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -22,5 +23,9 @@ public class StudentService {
 
     public void removeStudentById(Long id){
         studentRepository.deleteById(id);
+    }
+
+    public Optional<Student> findStudentById(Long id){
+        return studentRepository.findById(id);
     }
 }
